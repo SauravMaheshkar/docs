31bf855faa398b7e2d8acd01033691df0f7b8565
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2026-02-06 15:29:04 +0530

    fix: baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "/docs/",
+    baseUrl: "sauravmaheshkar.github.io/docs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
